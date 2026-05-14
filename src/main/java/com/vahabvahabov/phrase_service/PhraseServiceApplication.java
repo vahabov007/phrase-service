6ebba94219f48d049750506ee0c7a3577e8c7038
Vahab Vahabov <vahab.vahabov07@gmail.com>
2026-05-14 22:21:34 +0400

    feat : adding Kafka configuration.

--- src/main/java/com/vahabvahabov/phrase_service/PhraseServiceApplication.java
+++ src/main/java/com/vahabvahabov/phrase_service/PhraseServiceApplication.java
@@ -2,10 +2,9 @@ package com.vahabvahabov.phrase_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class PhraseServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/vahabvahabov/phrase_service/config/KafkaConsumerConfig.java
@@ -0,0 +1,43 @@
+package com.vahabvahabov.phrase_service.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "phrase-group");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
+
+        // SECURITY: Tell Spring which packages are allowed to be deserialized.
+        properties.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "com.vahabvahabov.*, java.util, java.lang");
+
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+
+
+}
